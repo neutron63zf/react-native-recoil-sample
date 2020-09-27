f5fc5a0cef5db78f5b43e1fe9f35ffeb57c71d3e
Nkowne63 <neutron63zf@gmail.com>
2020-09-27 20:12:46 +0900

    add: recoil counter sample

--- App.tsx
+++ App.tsx
@@ -1,19 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { RecoilRoot, useRecoilState, atom, selector } from "recoil";
+
+const countState = atom({
+  key: "sample/count", // 適当なユニークキー
+  default: 1, // デフォルト値
+});
+
+const liarCountState = selector({
+  key: "sample/liarCount",
+  get: ({ get }) => get(countState) * 2,
+  set: ({ get, set }, newValue) => set(countState, newValue),
+});
+
+const Counter = () => {
+  const [count, setCount] = useRecoilState(liarCountState);
+  return (
+    <>
+      <Button onPress={() => setCount((c) => c * 2)} title="Click Me!"></Button>
+      <Text>Clicked: {count}</Text>
+    </>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <RecoilRoot>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!!</Text>
+        <Counter />
+      </View>
+    </RecoilRoot>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+# memo
+
+npx create-react-native-app で expo を選択して、with typescript でテンプレートを入れて、https://github.com/facebookexperimental/Recoil/pull/114#issuecomment-699077480 に従って recoil nightly を入れてる。
+
+実機で確認するにはアプリストアから expo のアプリをまずインストールして、（Λ みたいなアイコンのやつ）、yarn start して出てきた QR コードをスキャンして開けばあとはよしなにやってくれる。ホットリロードも実機で効くので安心！
+
+Recoil のサンプルは https://sbfl.net/blog/2020/05/17/react-experimental-recoil-usage/ を参考にしてる
+
 # [TypeScript Example](https://www.typescriptlang.org/)
 
 <p>
@@ -12,7 +20,6 @@
 ```sh
 npx create-react-native-app -t with-typescript
 ```
-
 
 TypeScript is a superset of JavaScript which gives you static types and powerful tooling in Visual Studio Code including autocompletion and useful inline warnings for type errors.
 
